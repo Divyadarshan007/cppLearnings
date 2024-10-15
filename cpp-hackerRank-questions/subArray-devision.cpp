1b9b5ae3cf1fd5a4cd6acee3a92b541be8df49b3
divyadarshan007 <dasdivyadarshan@gmail.com>
2024-10-15 18:34:23 +0530

    cpp learnings

--- cpp-hackerRank-questions/subArray-devision.cpp
+++ cpp-hackerRank-questions/subArray-devision.cpp
@@ -2,15 +2,21 @@
 using namespace std;
 int main()
 {
-    int s[5] = {1, 1, 1, 1, 1};
-    int d = 3;
+    int s[1] = {4};
+    int d = 4;
 
-    int m = 2, count = 0;
+    int m = 1, count = 0;
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 1; i++)
     {
-        for (int j = 0; j < 5; j++)
+        for (int j = i + 1; j <= m; j++)
         {
+            cout << "hello";
+            if (size == 1)
+            {
+                count++;
+                return 0;
+            }
             if (s[i] + s[j] == d)
             {
                 if (count < m)
