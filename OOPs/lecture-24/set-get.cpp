9c5dc3de206c043cb87fa8c3013e8b0355ff0496
divyadarshan007 <dasdivyadarshan@gmail.com>
2024-11-13 17:48:04 +0530

    cpp learnings

---
+++ OOPs/lecture-24/set-get.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+private:
+    int age;
+    string name;
+    int rollNo;
+    string course;
+
+public:
+    void setData()
+    {
+
+        cout << "Enter Your Name : ";
+        getline(cin, this->name);
+        cout << "Enter Your Age : ";
+        cin >> this->age;
+        cout << "Enter Your Roll No : ";
+        cin >> this->rollNo;
+        cin.ignore();
+        cout << "Enter Your Course : ";
+        getline(cin, this->course);
+    }
+
+    void getData()
+    {
+        cout << "Name : " << this->name << endl;
+        cout << "Age : " << this->age << endl;
+        cout << "Roll No : " << this->rollNo << endl;
+        cout << "Course : " << this->course << endl;
+    }
+};
+
+int main()
+{
+
+    Student s1, s2, s3;
+
+    s1.setData();
+    s1.getData();
+    cout << endl;
+    cout << endl;
+    s2.setData();
+    s2.getData();
+    cout << endl;
+    cout << endl;
+    s3.setData();
+    s3.getData();
+
+    return 0;
+}
